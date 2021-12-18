ffd08179a0bcd402f0297d9c2532d85fb83fc71b
ChinthakuntlawarSaikrishna <96069758+ChinthakuntlawarSaikrishna@users.noreply.github.com>
2021-12-18 22:20:08 +0530

    commit

---
+++ index.js
@@ -0,0 +1,29 @@
+//document.write()
+//window.alert()
+//window.print()
+//console.log()
+/*function clickme() {
+    //document.write("My first Porgramme")
+    document.getElementsByClassName("mybox1")[0].innerText= "Hi";
+    document.getElementsByClassName("mybox1")[0].style.backgroundColor="skyblue"
+     console.log("My first js script")
+}
+ function doSomething() {
+    let a;
+    a=5;
+    var b;
+    b=5
+    let product=a*b;
+    let differerence=a-b;
+    let add=a+b;
+
+    console.log(product, differerence,add)
+}
+*/
+
+let a;
+a=5;
+let b;
+b=5;
+let c=a+b;
+console.log(c)
